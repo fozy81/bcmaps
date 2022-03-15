d13f1f411910d5b219884a3d0dcde7ba6c77fd85
fozy81 <fozy81@hotmail.com>
2022-03-15 09:56:00 +0000

    lint: line break

--- tests/testthat/test-data_functions.R
+++ tests/testthat/test-data_functions.R
@@ -5,7 +5,8 @@ avail <- available_layers()
 fn_names <- avail$layer_name[!is.na(avail$local)]
 
 
-test_that("test that all sf layer function work without error and returns an sf object as default", {
+test_that("test that all sf layer function work without error
+          and returns an sf object as default", {
   skip_on_cran()
   for (i in seq_along(fn_names)) {
     #cat("\n", fn_names[i]) #for debugging
